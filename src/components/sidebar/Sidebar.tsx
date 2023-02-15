888a6449a08fd0ed5eb3ccc2b766952b467c6ed9
Jonny Lin <jonny@snippyly.com>
2023-02-15 11:27:34 -0800

    Added selectable previews

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,15 +1,31 @@
 import styles from './Sidebar.module.css';
+import { useState } from 'react';
 
 import PlusIcon from '../../icons/plus.svg';
 import ThumbnailImage from '../../icons/thumbnail.png';
 
 const Sidebar = () => {
 
+  const [activePreview, setActivePreview] = useState(0);
+
+  const setActive = (idx: number) => {
+    setActivePreview(idx);
+  }
+
   return (
     <div className={styles['sidebar']}>
       <div className={styles['slide-previews']}>
-        <div className={styles['slide-preview']}>
+        <div onClick={() => setActive(0)} className={`${styles['slide-preview']} ${activePreview === 0 ? styles['active'] : null}`}>
           <p className={styles['slide-label']}>1</p>
+          <img src={ThumbnailImage} />
+          <p className={styles['thumbnail-text']}>Q1 Retrospective</p>
+        </div>
+        <div onClick={() => setActive(1)} className={`${styles['slide-preview']} ${activePreview === 1 ? styles['active'] : null}`}>
+          <p className={styles['slide-label']}>2</p>
+          <img src={ThumbnailImage} />
+        </div>
+        <div onClick={() => setActive(2)} className={`${styles['slide-preview']} ${activePreview === 2 ? styles['active'] : null}`}>
+          <p className={styles['slide-label']}>3</p>
           <img src={ThumbnailImage} />
         </div>
       </div>
